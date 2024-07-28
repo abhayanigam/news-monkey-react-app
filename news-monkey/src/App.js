4358d99313b10e8dbd2532ccab68229605e63c18
Abhaya Nigam <abhaya.nigam13@gmail.com>
2024-07-28 19:23:25 +0530

    Added files

--- news-monkey/src/App.js
+++ news-monkey/src/App.js
@@ -1,11 +1,20 @@
 import "./App.css";
 
 import React, { Component } from "react";
+import NavBar from "./components/NavBar";
+import News from "./components/News";
 
 //React component based class
 export default class App extends Component {
-  c = "Abhaya";
+  // render method renders series of methods , it also responsible to render
+  // jsx to html render
   render() {
-    return <>Hello my first class based component {this.c}</>;
+    // return <>Hello my first class based component {this.c}</>;
+    return (
+      <>
+        <NavBar />
+        <News />
+      </>
+    );
   }
 }
